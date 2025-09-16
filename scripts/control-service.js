83e48da4e428920efa6b0f90675d1b1c7161d1b1
Alexandr Martsul <aliaksandrm9321@gmail.com>
2025-09-16 23:20:52 +0300

    controll service

--- scripts/control-service.js
+++ scripts/control-service.js
@@ -1,22 +1,106 @@
 export class ControlService {
-  #nextPageLink = document.body.dataset.next
+  #nextPageLink = document.body.dataset.next;
+  #answerField = document.querySelector(".question__choice-input");
+  #availableAnswers = new Set(["Digit1", "Digit2", "Digit3", "Digit4"]);
+  #paragraphs = {
+    'F2': 1,
+    'F3': 2,
+    'F4': 3,
+    'F5': 4,
+    'F6': 5,
+    'F7': 6,
+    'F8': 7,
+    'F9': 8,
+    'F10': 9,
+  }
+  #activeParagraph = 1;
+  #questionParagraphsContainer = document.querySelector('.question-details');
+  #questionsParagraphs = document.querySelectorAll('.question__text-container');
+  #activeAudio = new Audio(this.#questionsParagraphs[0]?.dataset?.audio);
+  #timer = document.querySelector('#timer');
 
   init() {
     this.#initHandlers();
   }
 
   #initHandlers() {
-    document.addEventListener("keydown", event => {
-      const { code } = event;
+    document.addEventListener("keydown", (event) => {
+      const { code, key } = event;
       if (code === "Enter") {
-        this.#entreHandle()
+        this.#nextPage();
+      } else if (this.#availableAnswers.has(code)) {
+        this.#answerNumHandle(key);
+      } else if (code === "Digit5") {
+        this.#pauseSkip();
+      } else if (this.#paragraphs[code]) {
+        this.#changeParagraph(this.#paragraphs[code]);
+      } else if (code === 'F1') {
+        this.#playAudio();
+      } else if (code === 'F11') {
+        this.#stopAudio();
+      } else if (code === 'F12') {
+        event.preventDefault();
+        this.#toggleTimer();
+      } else if (code === 'Escape') {
+        event.preventDefault();
+        this.#closeExam();
       }
-    })
+    });
   }
 
-  #entreHandle() {
-    if (this.#nextPageLink) {
-      window.location.href = this.#nextPageLink;
+  #closeExam() {
+    window.location.href = "/preview-page.html";
+  }
+
+  #toggleTimer() {
+    if (this.#timer) {
+      this.#timer.classList.toggle('hidden');
     }
+  }
+
+  #nextPage() {
+    if (!(!this.#nextPageLink || (this.#answerField && !this.#answerField.value))) {
+      window.location.href = this.#nextPageLink;
+    } else if (this.#answerField && !this.#answerField.value) {
+      this.#answerField.classList.add('error');
+    }
+  }
+
+  #answerNumHandle(code) {
+    if (this.#answerField) {
+      this.#answerField.value = code;
+    }
+  }
+
+  #pauseSkip() {
+    if (document.querySelector(".pause")) {
+      window.location.href = "/preview-page.html";
+    }
+  }
+
+  #changeParagraph(num) {
+    const selectedParagraph = this.#questionsParagraphs[num];
+    if (this.#questionParagraphsContainer && selectedParagraph) {
+      this.#activeParagraph = num;
+      this.#scrollToActiveParagraph();
+      this.#changeActiveAudio()
+    }
+  }
+
+  #scrollToActiveParagraph() {
+    const activeParagraph = this.#questionsParagraphs[this.#activeParagraph - 1];
+    activeParagraph.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  #changeActiveAudio() {
+    this.#activeAudio = new Audio(this.#questionsParagraphs[this.#activeParagraph - 1].dataset.audio)
+  }
+
+  #playAudio() {
+    this.#activeAudio.play();
+  }
+
+  #stopAudio() {
+    this.#activeAudio.pause();
   }
 }
